d684f5e27e1005cf604c6116344a77129cd3a8b8
Lisethe <hello@lisethe.com>
2016-08-09 16:43:02 +0100

    transfer the serialise function into a module

--- modules/utils.js
+++ modules/utils.js
@@ -1,1 +1,15 @@
+const _ = require('lodash');
 
+module.exports.serialize = function serialize(obj) {
+	const object = obj;
+	return _.forEach(object, function(value, key) { // key = a; value = 1
+        if (typeof value == "function") {
+            return object[key] = value();
+        }
+
+        if (typeof value == "object") {
+            return object[key] = serialize(value);
+        }
+            object[key] = value;
+    })
+}
